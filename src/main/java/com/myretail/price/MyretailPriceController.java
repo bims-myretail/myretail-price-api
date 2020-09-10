d3f899d7290cfa37c8d68b2a8a79144ecfaf415d
bimlendushekhargit <bimlendu.shekhar.ind@gmail.com>
2020-09-10 13:49:57 +0530

    sonar

--- src/main/java/com/myretail/price/MyretailPriceController.java
+++ src/main/java/com/myretail/price/MyretailPriceController.java
@@ -2,9 +2,10 @@ package com.myretail.price;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class MyretailPriceController {
 
 	Logger logger = LoggerFactory.getLogger(MyretailPriceController.class);
 
-	@RequestMapping(path = "/price", method = RequestMethod.GET)
+	@GetMapping(path = "/price")
 	public PriceDTO findProductPrice(@RequestParam("productID") String id) {
 		logger.info("Fetching Price for peoduct ID : {}", id);
 		PriceDTO priceDTO = new PriceDTO();
@@ -24,7 +25,7 @@ public class MyretailPriceController {
 		return priceDTO;
 	}
 
-	@RequestMapping(path = "/price", method = RequestMethod.POST)
+	@PostMapping(path = "/price")
 	public PriceDTO updateProductPrice(@RequestBody PriceDTO priceDTO) {
 		logger.info("Updating Price for peoduct ID : {}", priceDTO.getProductID());
 		return priceDTO;
--- src/test/java/com/myretail/price/MyretailPriceApiApplicationTests.java
+++ src/test/java/com/myretail/price/MyretailPriceApiApplicationTests.java
@@ -1,13 +1,26 @@
 package com.myretail.price;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyretailPriceApiApplicationTests {
 
+	@Autowired
+	private MyretailPriceController controller;
+
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	public void testPriceResponse() throws Exception {
+		PriceDTO price = controller.findProductPrice("1");
+		assertThat(price.getValue()).isGreaterThan(1.0);
 	}
 
 }
